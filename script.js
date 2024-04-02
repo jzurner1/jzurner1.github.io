fba2ec899d72546a1d464a2eee463d5e013ddebd
jzurner1 <70181039+jzurner1@users.noreply.github.com>
2024-04-01 21:04:14 -0400

    Updated index

--- script.js
+++ script.js
@@ -25,3 +25,18 @@ document.addEventListener('DOMContentLoaded', function () {
     // Listen for the scroll event and update the navbar accordingly
     window.addEventListener('scroll', updateNavbar);
 });
+
+var coll = document.getElementsByClassName("skill-item");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
